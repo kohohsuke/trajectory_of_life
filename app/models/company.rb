daddcdb1ffc92aa30bdd050a7af50ea5724f9f46
kohohsuke <82505121+kohohsuke@users.noreply.github.com>
2021-05-31 15:37:43 +0900

    エラー文が日本語になるように修正

--- app/models/company.rb
+++ app/models/company.rb
@@ -4,7 +4,7 @@ class Company < ApplicationRecord
 
   with_options presence: true do
     validates :name
-    validates :post_code, format: { with: /\A\d{3}-\d{4}\z/ }
+    validates :post_code, format: { with: /\A\d{3}-\d{4}\z/, message: 'にはハイフン"-"を含めてください' }
     validates :address
     validates :website
     validates :characteristic
@@ -12,7 +12,7 @@ class Company < ApplicationRecord
     validates :second_reason
     validates :third_reason
     
-    with_options numericality: { other_than: 1 } do
+    with_options numericality: { other_than: 1, message: 'を選択してください' } do
       validates :category_id
       validates :occupation_id
     end

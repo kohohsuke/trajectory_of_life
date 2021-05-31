12fc58665c0d4a834efc38c4951a679a7af5eaf1
kohohsuke <82505121+kohohsuke@users.noreply.github.com>
2021-05-31 11:59:20 +0900

    companyモデルのバリデーションの設定

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,4 +2,17 @@ class Company < ApplicationRecord
 
   belongs_to :user
 
+  with_options presence :true do
+    validates :name
+    validates :post_code, format: { with: /\A\d{3}-\d{4}\z/ }
+    validates :address
+    validates :website
+    validates :category_id
+    validates :occupation_id
+    validates :characteristic
+    validates :first_reason
+    validates :second_reason
+    validates :third_reason
+  end
+
 end

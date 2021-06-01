176dc5377a08c9fe37fdaf909b72c47d9a79b021
kohohsuke <82505121+kohohsuke@users.noreply.github.com>
2021-06-01 15:11:11 +0900

    コメントモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_31_022449) do
+ActiveRecord::Schema.define(version: 2021_06_01_060747) do
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "text", null: false
+    t.bigint "user_id"
+    t.bigint "company_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["company_id"], name: "index_comments_on_company_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "companies", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -42,5 +52,7 @@ ActiveRecord::Schema.define(version: 2021_05_31_022449) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "companies"
+  add_foreign_key "comments", "users"
   add_foreign_key "companies", "users"
 end
